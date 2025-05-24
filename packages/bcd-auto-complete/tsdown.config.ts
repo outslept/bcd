b9fe997df52d509a6c7950c7313c6acd1e2c8258
outslept <webeugene@proton.me>
2025-05-25 01:35:21 +0300

    chore(`bcd-auto-complete`): setting up build configuration

--- packages/bcd-auto-complete/tsdown.config.ts
+++ packages/bcd-auto-complete/tsdown.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["./src/index.ts"],
+  entry: ["./index.ts"],
   target: "node20",
   clean: true,
   dts: true,
+  publint: true,
   platform: "neutral",
 });
