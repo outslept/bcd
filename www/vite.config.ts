3d3fd709d0a8c5bd7d6eab233010e9cc20fcac19
outslept <webeugene@proton.me>
2025-06-18 02:54:37 +0300

    chore: unocss was mising for some reason

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react-swc";
+import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
-import UnoCSS from 'unocss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
