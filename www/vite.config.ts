e483500169e660220cb3e7714fd57f98ce400388
outslept <135520429+outslept@users.noreply.github.com>
2025-07-21 03:00:30 +0300

    chore: removing unocss dependency

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,8 +1,6 @@
 import react from "@vitejs/plugin-react-swc";
-import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), UnoCSS()],
+  plugins: [react()],
 });
